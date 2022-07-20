49466bbe94e230a42b809ebf2794d879fc3a0a23
abattalov <abattalov14@gmail.com>
2022-07-19 22:18:03 -0500

    created database and added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+alex_bag = Bag.create(name: "Alex's Bag")
+jamie_bag = Bag.create(name: "Jamie's Bag")
+david_bag = Bag.create(name: "David's Bag")
+
+Disc.create(brand: "Innova", name: "Pig", speed: 3, glide: 1, turn: 0, fade: 3, plastic: "R-Pro", weight: 172, bag: alex_bag)
+Disc.create(brand: "Innova", name: "Valkyrie", speed: 9, glide: 4, turn: -2, fade: 2, plastic: "Halo", weight: 175, bag: alex_bag)
+Disc.create(brand: "Innova", name: "Mako3", speed: 5, glide: 5, turn: 0, fade: 0, plastic: "Champion", weight: 170, bag: alex_bag)
+
+Disc.create(brand: "Innova", name: "Destroyer", speed: 12, glide: 5, turn: -1, fade: 3, plastic: "Champion", weight: 172, bag: jamie_bag)
+Disc.create(brand: "Innova", name: "Roc3", speed: 5, glide: 4, turn: 0, fade: 3, plastic: "DX", weight: 172, bag: jamie_bag)
+Disc.create(brand: "Innova", name: "Pig", speed: 3, glide: 1, turn: 0, fade: 3, plastic: "R-Pro", weight: 175, bag: david_bag)
+
+
+puts "seeds planted!"
